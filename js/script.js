7ca5f6849152b9f706464279c1c90af771dd5cc9
zaidanki <44997660+zaidanki@users.noreply.github.com>
2018-11-13 04:00:18 -0500

    Update js/script.js
    Made it look a bit nicer.

--- js/script.js
+++ js/script.js
@@ -8,24 +8,24 @@ var quotes = [
 ];
 
 function getRandomQuote(quo){
-var q = 1;                                    //q is equal to one so that the first while loop does not run which means that quotes will never be repeated (untill they all been ran through)
-  while (p.indexOf(q) >= 0){                 //if the value is is there then a number >= 0 will generate and code will run to make sure it does not repeat quote.
+    var q = 1;                                    //q is equal to one so that the first while loop does not run which means that quotes will never be repeated (untill they all been ran through)
+    while (p.indexOf(q) >= 0){                 //if the value is is there then a number >= 0 will generate and code will run to make sure it does not repeat quote.
   q = Math.floor(Math.random()*quo.length); // generate a random number between 0 and the length of the selected function.
 }
-p.push(q);                   // this will only push in numbers that are not in the array becasue of the if function
+    p.push(q);                   // this will only push in numbers that are not in the array becasue of the if function
 if (p.length >= 5) {p = []}; // this is making sure that the array is reset once it hits 5 numbers
 return quo[q];              // returns the value we got, i did not give use .quote at the end in order to make the function reusable for source, citation, year, and so on.
 }
 
 function printQuote(){
-var gq = getRandomQuote(quotes) // this variable will make it easier to write the code and much neater.
-var yer;                    // a variable for adding year into the text
-var cit;                   // a variable for adding citation into the text
+    var gq = getRandomQuote(quotes) // this variable will make it easier to write the code and much neater.
+    var yer;                    // a variable for adding year into the text
+    var cit;                   // a variable for adding citation into the text
 if (gq.citation !== '')
 { cit =  '<span class="citation">' + gq.citation + '</span>' } else cit = ''; //conditional to make sure that citation has something in it, if not, then it will not show
 if (gq.year !== '')
 { yer =  '<span class="year">' + gq.year + '</span>' } else yer = '';    // same thing but with year
-var q = '<p class="quote">' + gq.quote + '</p>';                       // spliting up the text
+    var q = '<p class="quote">' + gq.quote + '</p>';                       // spliting up the text
     q += '<p class="source">' + gq.source + cit + yer + '</p>';
 
 document.getElementById('quote-box').innerHTML = q; // this will then make the quotes apear in the browser in the box itself.
